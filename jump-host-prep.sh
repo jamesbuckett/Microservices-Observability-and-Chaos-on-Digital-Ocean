88a6d5f94b0f8bbdf82c08b49c77f8312d02b098
jamesbuckett <james.buckett@gmail.com>
2020-09-26 12:36:48 +0800

    Updated doctl to v1.46.0

--- jump-host-prep.sh
+++ jump-host-prep.sh
@@ -8,8 +8,8 @@ sudo apt install -y python3-pip -y
 # doctl
 cd ~/ && rm -R ~/doctl
 cd ~/ && mkdir doctl && cd doctl
-curl -LO https://github.com/digitalocean/doctl/releases/download/v1.45.1/doctl-1.45.1-linux-amd64.tar.gz 
-tar -xvf doctl-1.45.1-linux-amd64.tar.gz
+curl -LO https://github.com/digitalocean/doctl/releases/download/v1.46.0/doctl-1.46.0-linux-amd64.tar.gz 
+tar -xvf doctl-1.46.0-linux-amd64.tar.gz
 sudo mv ~/doctl/doctl /usr/local/bin
 
 # Update .bashrc
@@ -21,13 +21,13 @@ echo "alias kga='kubectl get all'" >> ~/.bashrc
 # Update Message of the Day
 echo "Reference commands to the various URLs in this tutorial" >> /etc/motd
 echo "*************************************************************************************" >> /etc/motd
-echo "* Online Boutique is here: echo $BOUTIQUE_LB                                        *" >> /etc/motd
-echo "* Octant is here: echo $DROPLET_ADDR:8900                                           *" >> /etc/motd
-echo "* Grafana is here: echo $GRAFANA_LB                                                 *" >> /etc/motd
-echo "* Locust is here: echo $DROPLET_ADDR:8089                                           *" >> /etc/motd
-echo "* Start in another shell : octant &                                                 *" >> /etc/motd
-echo "* Start in another shell: locust --host="http://${BOUTIQUE_LB}" -u "${USERS:-10}" & *" >> /etc/motd
-echo "* Add this to .bashrc manually 'PS1='[\u@\h \w $(kube_ps1)]\$ '                     *" >> /etc/motd
+echo "* Online Boutique is here: echo $BOUTIQUE_LB  " >> /etc/motd
+echo "* Octant is here: echo $DROPLET_ADDR:8900  " >> /etc/motd
+echo "* Grafana is here: echo $GRAFANA_LB  " >> /etc/motd
+echo "* Locust is here: echo $DROPLET_ADDR:8089  " >> /etc/motd
+echo "* Start in another shell : octant &  " >> /etc/motd
+echo "* Start in another shell: locust --host="http://${BOUTIQUE_LB}" -u "${USERS:-10}" &  " >> /etc/motd
+echo "* Add this to .bashrc manually 'PS1='[\u@\h \w $(kube_ps1)]\$ '  " >> /etc/motd
 echo "*************************************************************************************" >> /etc/motd
 
 reboot
